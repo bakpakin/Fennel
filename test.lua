f2a3d3b91904ef9b3f154cc493fe8b56099a9fd8
Calvin Rose <calsrose@gmail.com>
2018-06-17 11:17:49 -0400

    One more IIFE test.

--- test.lua
+++ test.lua
@@ -195,6 +195,8 @@ local cases = {
         ["(let [(a b c d e f g) (if (= (+ 1 1) 3) nil ((or unpack table.unpack) [1 2 3 4 5 6 7]))] (+ a b c d e f g))"]=28,
         -- IIFE if test v3
         ["(# [(if (= (+ 1 1) 2) (values 1 2 3 4 5) (values 1 2 3))])"]=5,
+        -- IIFE if test v4
+        ["(select \"#\" (if (= 1 (- 3 2)) (values 1 2 3 4 5) :onevalue))"]=5,
         -- Values special in array literal
         ["(# [(values 1 2 3 4 5)])"]=5
     },

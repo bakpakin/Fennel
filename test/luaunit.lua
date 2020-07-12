a47d5891277177a9d6c333cf0d955c0fd921ce59
Phil Hagelberg <phil@hagelb.org>
2020-07-12 10:06:18 -0700

    Work around bug in luaunit with Lua 5.4
        Testing lua 5.4:
        lua5.4: ./test/luaunit.lua:212: bad argument #1 to 'randomseed' (number has no integer representation)
        stack traceback:
        	[C]: in function 'math.randomseed'
        	./test/luaunit.lua:212: in main chunk
        	[C]: in function 'require'
        	test/init.lua:7: in main chunk
        	[C]: in ?
        make: *** [Makefile:21: testall] Error 1

--- test/luaunit.lua
+++ test/luaunit.lua
@@ -209,7 +209,7 @@ end
 M.private.sortedPairs = sortedPairs
 
 -- seed the random with a strongly varying seed
-math.randomseed(os.clock()*1E11)
+math.randomseed(math.floor(os.clock()*1E11))
 
 local function randomizeTable( t )
     -- randomize the item orders of the table t

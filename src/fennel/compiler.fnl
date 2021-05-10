bakpakin/Fennel

commit b43fe48c890fdbd788ff17051c843fd367a68a91

Add symmeta to declared locals later to work around shadowing problem
When you shadow an existing local, the right-hand side is compiled
after the local is declared, not before. The emitted code is still
correct, but the usage-tracking is incorrect.

The problem is that there is a circular dependency between the right
hand side and the left hand side. The RHS must know the names of the
locals it's getting bound to (see opts.target) but it also must be
compiled without those locals really being in scope yet, because those
locals aren't really "active" until after that binding has been
established.

This basically only matters for linting. The following code compiles
correctly, but fails to lint without this patch.

    (fn [abc] (let [abc abc] abc))

Without this patch the third abc gets treated as a reference to the
let-bound abc, not the function parameter.

Deferring the creation of the symmeta table allows the right-hand side
to compile without triggering the bug as far as pertains to usage tracking.

Fixes https://todo.sr.ht/~technomancy/fennel/12

src/fennel/compiler.fnl
@@ -570,7 +570,9 @@ which we have to do if we don't know."
         (assert-compile (not (and nomulti (utils.multi-sym? raw)))
                        (.. "unexpected multi symbol " raw) up1)
         (if declaration
-            (declare-local symbol {:var isvar} scope symbol new-manglings)
+            ;; Technically this is too early to declare the local, but leaving
+            ;; out the meta table and setting it later works around the problem.
+            (declare-local symbol nil scope symbol new-manglings)
             (let [parts (or (utils.multi-sym? raw) [raw])
                   meta (. scope.symmeta (. parts 1))]
               (when (and (= (# parts) 1) (not forceset))
@@ -624,7 +626,11 @@ which we have to do if we don't know."
             (check-binding-valid left scope left)
             (if top
                 (compile-top-target [lname])
-                (emit parent (setter:format lname (exprs1 rightexprs)) left)))
+                (emit parent (setter:format lname (exprs1 rightexprs)) left))
+            ;; We have to declare meta for the left *after* compiling the right
+            ;; see https://todo.sr.ht/~technomancy/fennel/12
+            (when declaration
+              (tset scope.symmeta (utils.deref left) {:var isvar})))
           (utils.table? left) ; table destructuring
           (let [s (gensym scope)]
             (var right (if top
@@ -659,8 +665,6 @@ which we have to do if we don't know."
           (let [(left-names tables) (values [] [])]
             (each [i name (ipairs left)]
               (if (utils.sym? name) ; binding directly to a name
-                  ;; TODO: this is too early to declare the local; see
-                  ;; https://todo.sr.ht/~technomancy/fennel/12
                   (table.insert left-names (getname name up1))
                   (let [symname (gensym scope)]
                     ;; further destructuring of tables inside values
@@ -671,6 +675,9 @@ which we have to do if we don't know."
                 (let [lvalue (table.concat left-names ", ")
                       setting (setter:format lvalue (exprs1 rightexprs))]
                   (emit parent setting left)))
+            (when declaration
+              (each [_ sym (ipairs left)]
+                (tset scope.symmeta (utils.deref sym) {:var isvar})))
             ;; recurse if left-side tables found
             (each [_ pair (utils.stablepairs tables)]
               (destructure1 (. pair 1) [(. pair 2)] left)))

commit ba7c56d2227f95b15d29cebe95325a032d92ead4

Fix destructuring kv tables inside values
When symmeta is added in destructure-values, we have to check that the
value is in fact a sym (not a table). Any names declared in the table
destructure have their symmeta added in the recursive call to
destructure1.

This manifests as a compiler error:

    (let [({:x 10}) {:x 10}] x) => Error: table index is nil

... from setting a table key to (utils.deref {:x 10}), which is nil.

Though note that sequential table destructuring accidentally worked,
since utils.deref is just (. sym 1), or in this case, `x`:

    (let [([x]) [10]] x) => 10

This appears to be a regression from b43fe48.

src/fennel/compiler.fnl
@@ -722,7 +722,8 @@ which we have to do if we don't know."
         (compile-top-target left-names)
         (when declaration
           (each [_ sym (ipairs left)]
-            (tset scope.symmeta (utils.deref sym) {:var isvar})))
+            (when (utils.sym? sym)
+              (tset scope.symmeta (utils.deref sym) {:var isvar}))))
         ;; recurse if left-side tables found
         (each [_ pair (utils.stablepairs tables)]
           (destructure1 (. pair 1) [(. pair 2)] left))))

test/core.fnl
@@ -202,7 +202,8 @@
                "(local [a b c &as t] [1 2 3]) (+ c (. t 2))" 5
                "(local (-a -b) ((fn [] (values 4 29)))) (+ -a -b)" 33
                "(var [a [b c]] [1 [2 3]]) (set a 2) (set c 8) (+ a b c)" 12
-               "(var x 0) (each [_ [a b] (ipairs [[1 2] [3 4]])] (set x (+ x (* a b)))) x" 14}]
+               "(var x 0) (each [_ [a b] (ipairs [[1 2] [3 4]])] (set x (+ x (* a b)))) x" 14
+               "(let [({: x} y) (values {:x 10} 20)] (+ x y))" 30}]
     (each [code expected (pairs cases)]
       (l.assertEquals (fennel.eval code {:correlate true}) expected code))))
 
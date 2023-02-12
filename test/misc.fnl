bakpakin/Fennel

commit 764dfd3d529c4842bd857fdf3c09e7f8baff9a91

Fix compile-table to use ipairs instead of for.
This will ensure that the sequential part of a table stops at the
first nil and the rest is emitted as key/value.

src/fennel/compiler.fnl
@@ -414,18 +414,10 @@ if opts contains the nval option."
       (set (src.bytestart src.byteend) (values bytestart byteend))))
   (= :table (type node)))
 
-(fn max-n [t]
-  "A hacky way to obtain semi-real lable length."
-  (var n 0)
-  (each [k (pairs t)]
-    (when (and (= :number (type k)))
-      (set n (math.max k n))))
-  n)
-
 (fn quote-literal-nils [index node parent]
   "Replaces literal `nil` values with quoted version."
   (when (and parent (utils.list? parent))
-    (for [i 1 (max-n parent)]
+    (for [i 1 (utils.maxn parent)]
       (match (. parent i)
         nil (tset parent i (utils.sym "nil")))))
   (values index node parent))
@@ -552,29 +544,20 @@ if opts contains the nval option."
     (handle-compile-opts [(utils.expr (serialize ast) :literal)] parent opts)))
 
 (fn compile-table [ast scope parent opts compile1]
-  (let [buffer []
-        max (if (= nil (. ast 1)) 0 (utils.maxn ast))]
-    (fn write-other-values [k]
-      (when (or (not= (type k) :number) (not= (math.floor k) k) (< k 1)
-                (< max k))
-        (if (and (= (type k) :string) (utils.valid-lua-identifier? k)) [k k]
-            (let [[compiled] (compile1 k scope parent {:nval 1})
-                  kstr (.. "[" (tostring compiled) "]")]
-              [kstr k]))))
+  (fn escape-key [k]
+    (if (and (= (type k) :string) (utils.valid-lua-identifier? k))
+        k
+        (let [[compiled] (compile1 k scope parent {:nval 1})]
+          (.. "[" (tostring compiled) "]"))))
 
-    (let [keys (icollect [k v (utils.stablepairs ast)]
-                 (write-other-values k v))]
-      (utils.map keys
-                 (fn [[k1 k2]]
-                   (let [[v] (compile1 (. ast k2) scope parent {:nval 1})]
-                     (string.format "%s = %s" k1 (tostring v))))
-                 buffer))
+  (let [buffer (icollect [i elem (ipairs ast)]
+                 (let [nval (and (not= nil (. ast (+ i 1))) 1)]
+                   (exprs1 (compile1 elem scope parent {: nval}))))]
 
-    ;; write numeric keyed values
-    (for [i 1 max]
-      (let [nval (and (not= i max) 1)]
-        (table.insert buffer
-                      (exprs1 (compile1 (. ast i) scope parent {: nval})))))
+    (icollect [k v (utils.stablepairs ast) :into buffer]
+      (if (not (. buffer k)) ; not part of the sequence section above
+          (let [[v] (compile1 (. ast k) scope parent {:nval 1})]
+            (string.format "%s = %s" (escape-key k) (tostring v)))))
 
     (handle-compile-opts [(utils.expr (.. "{" (table.concat buffer ", ") "}")
                                       :expression)]

test/core.fnl
@@ -189,7 +189,9 @@
     (each [code expected (pairs cases)]
       (l.assertEquals (fennel.eval code {:correlate true}) expected code))
     (when (not _G.getfenv)
-      (l.assertEquals (fennel.eval "(type _ENV)") :table))))
+      (l.assertEquals (fennel.eval "(type _ENV)") :table))
+    ;; ensure sparse tables don't print with a ton of nils in them
+    (l.assertNil (string.find (fennel.compileString "{1 :a 999999 :b}") :nil))))
 
 (fn test-if []
   (let [cases {"(do (fn myfn [x y z] (+ x y z)) (myfn 1 (if 1 (values 2 5) 3) 4))" 7

commit 4c82772e0e7289ab29ab12ad890a9bbea2613bf0

Fix a bug which would cause certain table keys to be omitted.
Luckily this bug never made it into a release but was introduced in 764dfd3.

src/fennel/compiler.fnl
@@ -551,12 +551,14 @@ if opts contains the nval option."
         (let [[compiled] (compile1 k scope parent {:nval 1})]
           (.. "[" (tostring compiled) "]"))))
 
-  (let [buffer (icollect [i elem (ipairs ast)]
+  (let [keys []
+        buffer (icollect [i elem (ipairs ast)]
                  (let [nval (and (not= nil (. ast (+ i 1))) 1)]
+                   (tset keys i true)
                    (exprs1 (compile1 elem scope parent {: nval}))))]
 
     (icollect [k v (utils.stablepairs ast) :into buffer]
-      (if (not (. buffer k)) ; not part of the sequence section above
+      (if (not (. keys k)) ; not part of the sequence section above
           (let [[v] (compile1 (. ast k) scope parent {:nval 1})]
             (string.format "%s = %s" (escape-key k) (tostring v)))))
 

test/misc.fnl
@@ -134,11 +134,19 @@
     (l.assertFalse (fennel.eval method-code))
     (l.assertFalse (fennel.eval comparator-code))))
 
+(fn test-table []
+  (let [code "{:transparent 0 :sky 0 :sun 1 :stem 2 :cloud 3 :star 3 :moon 3
+ :cloud-2 4 :gray 4 :rain 5 :butterfly-body 6 :bee-body-1 6 :white 3
+ :butterfly-eye 7 :bee-body-2 7 :dying-plant 7 8 8 9 9}"
+        tbl (fennel.eval code)]
+    (l.assertEquals (. tbl 8) 8)))
+
 {: test-empty-values
  : test-env-iteration
  : test-global-mangling
  : test-include
  : test-leak
+ : test-table
  : test-runtime-quote
  : test-traceback
  : test-short-circuit}